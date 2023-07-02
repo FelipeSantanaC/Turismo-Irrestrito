20de3ed43ac69cc5eb4fd46e4609916a679c27f8
FelipeSantanaC <felipecabocloo@gmail.com>
2023-07-02 15:53:33 -0300

    finalizacao do cadastro

--- authRst/myapp/static/js/app.js
+++ authRst/myapp/static/js/app.js
@@ -10,3 +10,33 @@ registerButton.addEventListener('click', () =>{
 closeRegisterButton.addEventListener('click', () =>{
     modal.close();
 })
+
+function cadastrar() {
+    var nomeCompleto = document.getElementById('nomeCompleto').value;
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+  
+    var userData = {
+      name: nomeCompleto,
+      email: email,
+      password: senha
+    };
+  
+    // Envia uma solicitação POST para a API do Django
+    fetch('/api/myapp/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      window.location.href = '/';
+    })
+    .catch(error => {
+      console.error('Erro ao cadastrar usuário:', error);
+    });
+  }
+  
+  document.getElementById('buttonCadastrar').addEventListener('click', cadastrar);
