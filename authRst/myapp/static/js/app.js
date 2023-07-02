345e4c728da871b89cdca6a70c3a72014672afb1
FelipeSantanaC <felipecabocloo@gmail.com>
2023-07-02 16:02:06 -0300

    finalizacao cadastro

--- authRst/myapp/static/js/app.js
+++ authRst/myapp/static/js/app.js
@@ -10,3 +10,39 @@ registerButton.addEventListener('click', () =>{
 closeRegisterButton.addEventListener('click', () =>{
     modal.close();
 })
+
+// Função que é chamada quando o botão de cadastro é clicado
+function cadastrar() {
+  // Obtém os valores dos campos de entrada
+  var nomeCompleto = document.getElementById('nomeCompleto').value;
+  var email = document.getElementById('email').value;
+  var senha = document.getElementById('senha').value;
+
+  // Cria um objeto com os dados do usuário
+  var userData = {
+    name: nomeCompleto,
+    email: email,
+    password: senha
+  };
+
+  // Envia uma solicitação POST para a API do Django
+  fetch('/api/myapp/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userData)
+  })
+  .then(response => response.json())
+  .then(data => {
+    // O cadastro foi bem-sucedido, redirecione para a página index
+    window.location.href = '/';
+  })
+  .catch(error => {
+    // Ocorreu um erro ao cadastrar o usuário
+    console.error('Erro ao cadastrar usuário:', error);
+  });
+}
+
+// Adicione um ouvinte de evento ao botão de cadastro
+document.getElementById('buttonCadastrar').addEventListener('click', cadastrar);
