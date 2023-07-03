10e4430b12bafb25c485293bd866429b4b527155
pablorenato1 <pablorenato18@gmail.com>
2023-07-03 19:00:48 -0300

    Resolvendo o problema do Pop Up de Cadastro

--- authRst/myapp/static/js/app.js
+++ authRst/myapp/static/js/app.js
@@ -2,6 +2,10 @@ const registerButton = document.querySelector('#register-button');
 const closeRegisterButton = document.querySelector('#closeButtonCadastro');
 const modal = document.querySelector('#modalCadastro')
 
+const loginButton = document.querySelector('#login-button');
+const closeLoginButton = document.querySelector('#closeButtonLogin');
+const modalLogin = document.querySelector('#modalLogin')
+
 registerButton.addEventListener('click', () =>{
     modal.showModal();
 
@@ -11,9 +15,6 @@ closeRegisterButton.addEventListener('click', () =>{
     modal.close();
 })
 
-const loginButton = document.querySelector('#login-button');
-const closeLoginButton = document.querySelector('#closeButtonLogin');
-const modalLogin = document.querySelector('#modalLogin')
 
 loginButton.addEventListener('click', () =>{
   modalLogin.showModal();
@@ -24,32 +25,46 @@ closeLoginButton.addEventListener('click', () =>{
   modalLogin.close();
 })
 
+function ValidateEmail(mail) {
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return (true)
+  }
+    alert("Você esta inserindo email inválido!")
+    return (false)
+}
+
 function cadastrar() {
     var nomeCompleto = document.getElementById('nomeCompleto').value;
     var email = document.getElementById('email').value;
     var senha = document.getElementById('senha').value;
   
-    var userData = {
-      name: nomeCompleto,
-      email: email,
-      password: senha
-    };
-  
-    // Envia uma solicitação POST para a API do Django
-    fetch('/api/myapp/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(userData)
-    })
-    .then(response => response.json())
-    .then(data => {
-      window.location.href = '/';
-    })
-    .catch(error => {
-      console.error('Erro ao cadastrar usuário:', error);
-    });
+
+    if (!ValidateEmail(email)) {
+      return false
+    } else {
+      var userData = {
+        name: nomeCompleto,
+        email: email,
+        password: senha
+      };
+    
+      // Envia uma solicitação POST para a API do Django
+      fetch('/api/myapp/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+      })
+      .then(response => response.json())
+      .then(data => {
+        window.location.href = '/';
+      })
+      .catch(error => {
+        console.error('Erro ao cadastrar usuário:', error);
+      });
+    }
   }
   
   document.getElementById('buttonCadastrar').addEventListener('click', cadastrar);
