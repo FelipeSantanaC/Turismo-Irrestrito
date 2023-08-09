45e2ce0e61ad537d52fa1110e0b7d3301f242322
FelipeSantanaC <felipecabocloo@gmail.com>
2023-08-09 19:46:55 -0300

    model de preferencia

--- authRst/authRst/settings.py
+++ authRst/authRst/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     'myapp.apps.MyappConfig',
     'corsheaders',
+    'recommendations.apps.RecommendationsConfig',
 ]
 
 MIDDLEWARE = [
--- authRst/recommendations/models.py
+++ authRst/recommendations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Preferencia(models.Model):
+    cluster_id = models.IntegerField()
+    local1 = models.IntegerField()
+    local2 = models.IntegerField()
+    local3 = models.IntegerField()
+    local4 = models.IntegerField()
+
+    def __str__(self):
+        return f"Preferencia do Cluster {self.cluster_id}"
