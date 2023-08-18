0062561fc1094a1af5d565bd7ffd8549b21fa9ea
grsmth <moisesvictor15@gmail.com>
2023-08-18 09:59:05 -0300

    Update models.py
    - It was added two new classes: Rating and Post

--- authRst/local_interactions/models.py
+++ authRst/local_interactions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from myapp.models import MyUser, Local
 
-# Create your models here.
+class Rating(models.Model):       
+    user_id = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    local_id = models.ForeignKey(Local, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+class Post(models.Model): 
+    user_id = models.ForeignKey(MyUser, on_delete=models.CASCADE) # further create a get to retrieve its profile picture and name from user profile
+    local_id = models.ForeignKey(Local, on_delete=models.CASCADE)
+    rating_id = models.ForeignKey(Rating, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+
